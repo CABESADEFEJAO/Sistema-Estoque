f611c11e8c41913272f80cd3d303182ebefbbc9c
Natal Souza Batista <134715965+CABESADEFEJAO@users.noreply.github.com>
2025-11-24 20:52:15 -0300

    Subindo primeira versão

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const cors = require('cors');
+const db = require('./db');
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get('/produtos', async (req,res)=>{
+  const {rows}= await db.query('SELECT * FROM produtos ORDER BY id');
+  res.json(rows);
+});
+
+app.post('/produtos', async (req,res)=>{
+  const {nome, quantidade}= req.body;
+  const {rows}= await db.query(
+    'INSERT INTO produtos (nome, quantidade) VALUES ($1,$2) RETURNING *',
+    [nome, quantidade]
+  );
+  res.json(rows[0]);
+});
+
+app.put('/produtos/:id', async (req,res)=>{
+  const {id}= req.params;
+  const {nome, quantidade}= req.body;
+  const {rows}= await db.query(
+    'UPDATE produtos SET nome=$1, quantidade=$2 WHERE id=$3 RETURNING *',
+    [nome, quantidade, id]
+  );
+  res.json(rows[0]);
+});
+
+app.delete('/produtos/:id', async (req,res)=>{
+  const {id}= req.params;
+  const {rows}= await db.query(
+    'DELETE FROM produtos WHERE id=$1 RETURNING *',[id]
+  );
+  res.json({deleted: rows[0]});
+});
+
+app.listen(3000,()=>console.log('API rodando na 3000'));
